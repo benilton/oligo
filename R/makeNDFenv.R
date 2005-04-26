a7241763c858248eccbddc2086ba97835cc83a87
Benilton Carvalho <bcarvalh@jhsph.edu>
2005-04-26 04:38:37 +0000

    Removed unnecessary functions
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligo@12045 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/makeNDFenv.R
+++ R/makeNDFenv.R
@@ -2,11 +2,10 @@
 # Author: Benilton Carvalho
 # Date: Mar / 2005
 
-ndfenv <- function(ndffile){
+makeNDFenv <- function(ndffile){
   
   cat(paste("Reading",ndffile,"\n"))
   ndf <- readndf(ndffile)
-  ndf$index <- max(ndf$Y)*ndf$X + ndf$Y
   envndf <- new.env()
 
   vars <- names(ndf)
@@ -14,21 +13,12 @@ ndfenv <- function(ndffile){
     assign(vars[i],ndf[,i],envndf)
   }
   
-  # Creating class "platformDesign" with 3 slots (environment, maker and seed)
-  # Using seed, we can have a 1:1 mapping between index and (X,Y)
-  # index = seed * X + Y
-  # NDF information
-  # type: expression/SNP
-  
   setClass("platformDesign",
            representation(featureInfo = "environment",
                           manufacturer = "character",
-                          seed = "numeric",
                           type = "character"))
   new("platformDesign",
       featureInfo=envndf,
       manufacturer="NimbleGen",
-      seed=max(ndf$Y),
-      type="expression")
-  
+      type="expression")  
 }
--- R/pm.R
+++
@@ -1,6 +0,0 @@
-# Returning the expression for PMs
-# Author: Benilton Carvalho
-# Date: Apr/05
-
-#pm <- function(oligB){
-#  getDesignInfo(oligB)
