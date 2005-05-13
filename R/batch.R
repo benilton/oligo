34eeee619da587811c512606181751b16b815d21
Benilton Carvalho <bcarvalh@jhsph.edu>
2005-05-13 18:22:23 +0000

    Updated version to work jointly with makeChipDesign
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligo@12401 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/batch.R
+++ R/batch.R
@@ -12,70 +12,70 @@ setClass("oligoBatch",
          contains="eSet")
 
 if (is.null(getGeneric("designName")))
-  setGeneric("designName",function(obj) standardGeneric("designName"))
-setMethod("designName","oligoBatch", function(obj) obj@designName)
+  setGeneric("designName",function(object) standardGeneric("designName"))
+setMethod("designName","oligoBatch", function(object) object@designName)
 
 if (is.null(getGeneric("featureInfo")))
-  setGeneric("featureInfo", function(obj) standardGeneric("featureInfo"))
-setMethod("featureInfo","oligoBatch", function(obj) slot(get(designName(obj)),"featureInfo"))
+  setGeneric("featureInfo", function(object) standardGeneric("featureInfo"))
+setMethod("featureInfo","oligoBatch", function(object) slot(get(designName(object)),"featureInfo"))
 
 if (is.null(getGeneric("featureType")))
-  setGeneric("featureType", function(obj) standardGeneric("featureType"))
-setMethod("featureType","oligoBatch", function(obj) get("feature_type", envir = featureInfo(obj)))
+  setGeneric("featureType", function(object) standardGeneric("featureType"))
+setMethod("featureType","oligoBatch", function(object) get("feature_type", envir = featureInfo(object)))
 
 if (is.null(getGeneric("featureClass")))
-  setGeneric("featureClass", function(obj) standardGeneric("featureClass"))
-setMethod("featureClass","oligoBatch", function(obj) get("feature_class", envir = featureInfo(obj)))
+  setGeneric("featureClass", function(object) standardGeneric("featureClass"))
+setMethod("featureClass","oligoBatch", function(object) get("feature_class", envir = featureInfo(object)))
 
 
 if (is.null(getGeneric("featureNames")))
-  setGeneric("featureNames", function(obj) standardGeneric("featureNames"))
+  setGeneric("featureNames", function(object) standardGeneric("featureNames"))
 setMethod("featureNames","oligoBatch",
-          function(obj){
-            pmfeaturenames <- get("feature_names", envir = featureInfo(obj))[featureType(obj) == "PM"]
-            return(pmfeaturenames[order(pmfeatureGroup(obj))])
+          function(object){
+            pmfeaturenames <- get("feature_names", envir = featureInfo(object))[featureType(object) == "PM"]
+            return(pmfeaturenames[order(pmfeatureGroup(object))])
           })
 
 if (is.null(getGeneric("featureGroup")))
-  setGeneric("featureGroup", function(obj) standardGeneric("featureGroup"))
-setMethod("featureGroup","oligoBatch", function(obj) get("feature_group", envir = featureInfo(obj)))
+  setGeneric("featureGroup", function(object) standardGeneric("featureGroup"))
+setMethod("featureGroup","oligoBatch", function(object) get("feature_group", envir = featureInfo(object)))
 
 if (is.null(getGeneric("pmfeatureGroup")))
-  setGeneric("pmfeatureGroup", function(obj) standardGeneric("pmfeatureGroup"))
-setMethod("pmfeatureGroup","oligoBatch", function(obj) featureGroup(obj)[featureType(obj) == "PM"])
+  setGeneric("pmfeatureGroup", function(object) standardGeneric("pmfeatureGroup"))
+setMethod("pmfeatureGroup","oligoBatch", function(object) featureGroup(object)[featureType(object) == "PM"])
 
 if (is.null(getGeneric("mmfeatureGroup")))
-  setGeneric("mmfeatureGroup", function(obj) standardGeneric("mmfeatureGroup"))
-setMethod("mmfeatureGroup","oligoBatch", function(obj) featureGroup(obj)[featureType(obj) == "MM"])
+  setGeneric("mmfeatureGroup", function(object) standardGeneric("mmfeatureGroup"))
+setMethod("mmfeatureGroup","oligoBatch", function(object) featureGroup(object)[featureType(object) == "MM"])
 
 if (is.null(getGeneric("mismatch")))
-  setGeneric("mismatch", function(obj) standardGeneric("mismatch"))
-setMethod("mismatch","oligoBatch", function(obj) get("mismatch", envir = featureInfo(obj)))
+  setGeneric("mismatch", function(object) standardGeneric("mismatch"))
+setMethod("mismatch","oligoBatch", function(object) get("mismatch", envir = featureInfo(object)))
 
 ### PM
 if (is.null(getGeneric("pm")))
-  setGeneric("pm",function(obj,...) standardGeneric("pm"))
+  setGeneric("pm",function(object,...) standardGeneric("pm"))
 setMethod("pm","oligoBatch",
-          function(obj) {
-            pmnames <- featureNames(obj)[featureType(obj) == "PM"]
-            pms <- exprs(obj)[featureType(obj) == "PM",]
+          function(object) {
+            pmnames <- featureNames(object)[featureType(object) == "PM"]
+            pms <- exprs(object)[featureType(object) == "PM",]
             rownames(pms) <- pmnames
-            pms <- pms[order(pmfeatureGroup(obj)),]
+            pms <- pms[order(pmfeatureGroup(object)),]
             return(pms)
           })
 
 ### MM
 if (is.null(getGeneric("mm")))
-  setGeneric("mm",function(obj,...) standardGeneric("mm"))
+  setGeneric("mm",function(object,...) standardGeneric("mm"))
 setMethod("mm","oligoBatch",
-          function(obj) {
-            maxmm <- max(mismatch(obj))
-            mmnames <- featureNames(obj)[featureType(obj) == "MM"]
+          function(object) {
+            maxmm <- max(mismatch(object))
+            mmnames <- featureNames(object)[featureType(object) == "MM"]
             mms <- list()
             for (i in 1:maxmm){
-              mmsl <- exprs(obj)[featureType(obj) == "MM" & mismatch(obj) == i,]
+              mmsl <- exprs(object)[featureType(object) == "MM" & mismatch(object) == i,]
               rownames(mmsl) <- mmnames
-              mmsl <- mmsl[order(mmfeatureGroup(obj)),]
+              mmsl <- mmsl[order(mmfeatureGroup(object)),]
               mms[[i]] <- mmsl 
             }
             if (maxmm == 1) mms <- mms[[1]]
