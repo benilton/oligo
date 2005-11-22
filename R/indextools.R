5816db7fa83418e1ee3374aea7d449c319d2ee38
Benilton Carvalho <bcarvalh@jhsph.edu>
2005-11-22 03:05:28 +0000

    BC: 21 Nov 2005 - added i2xy and xy2i
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligo@15063 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/indextools.R
@@ -0,0 +1,11 @@
+i2xy <- function(i,obatch){
+  xy <- mget(c("X","Y"),envir=featureInfo(getPD(obatch)))
+  return(cbind(xy$X[i],xy$Y[i]))
+}
+
+xy2i <- function(x,y,obatch){
+  xy <- mget(c("X","Y"),envir=featureInfo(getPD(obatch)))
+  xy1 <- xy$X+(xy$Y-1)*max(xy$X)
+  xy2=x+(y-1)*max(xy$X)
+  match(xy2,xy1)
+}
