b1b58d5c8595efec63113a5e61b75ac4e4ac46ac
Benilton Carvalho <bcarvalh@jhsph.edu>
2005-12-10 02:13:58 +0000

    BC: Dec 9, 2005 - changed gcrma_getSeq2/sequenceDesignMatrix, so it accepts general lengths
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligo@15306 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/baseProfile.c
+++ src/baseProfile.c
@@ -6,8 +6,8 @@
 #include <string.h>
 #define STR(SE) CHAR(STRING_ELT(SE,0))
 
-SEXP gcrma_getSeq2(SEXP,SEXP);
-SEXP gcrma_getSeq2(SEXP psequence, SEXP x) {
+SEXP gcrma_getSeq2(SEXP,SEXP,SEXP);
+SEXP gcrma_getSeq2(SEXP psequence, SEXP x, SEXP length) {
     SEXP outMatrix;
     char *pseq;
     int k,i;
@@ -15,23 +15,23 @@ SEXP gcrma_getSeq2(SEXP psequence, SEXP x) {
     K=INTEGER(x)[0];
     pseq=STR(psequence);
 
-    PROTECT(outMatrix = allocMatrix(INTSXP, K, 75));
+    PROTECT(outMatrix = allocMatrix(INTSXP, K, (3*length)));
         for(k=0;k<K;k++){
-      for (i = 0; i < 25; i++) {
-	if (pseq[k*25+i] == 'A')
+      for (i = 0; i < length; i++) {
+	if (pseq[k*length+i] == 'A')
 	  INTEGER(outMatrix)[i*K+k]=1;
 	else
 	  INTEGER(outMatrix)[i*K+k]=0;
 	
-	if (pseq[k*25+i] == 'C')
-	  INTEGER(outMatrix)[(i+25)*K+k] = 1;
+	if (pseq[k*length+i] == 'C')
+	  INTEGER(outMatrix)[(i+length)*K+k] = 1;
 	else
-	  INTEGER(outMatrix)[(i+25)*K+k] = 0;
+	  INTEGER(outMatrix)[(i+length)*K+k] = 0;
 	
-	if (pseq[k*25+i] == 'G')
-	  INTEGER(outMatrix)[(i+50)*K+k] = 1;
+	if (pseq[k*length+i] == 'G')
+	  INTEGER(outMatrix)[(i+(2*length))*K+k] = 1;
 	else
-	  INTEGER(outMatrix)[(i+50)*K+k] = 0;
+	  INTEGER(outMatrix)[(i+(2*length))*K+k] = 0;
       }
       }
     /*  for (i=0;i<strlen(pseq);i++){
