a307865d48d31b51edbbafd5668cc09e3d6b8b79
Rafael Irizarry <ririzarr@jhsph.edu>
2005-12-12 18:25:46 +0000

    RI: took out argument. the length can be figured out
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligo@15336 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/sequence.R
+++ R/sequence.R
@@ -1,10 +1,10 @@
 pmSequence <- function(x)  getPlatformDesign(x)$sequence[pmindex(x)]
 mmSequence <- function(x)  getPlatformDesign(x)$sequence[mmindex(x)]
 
-sequenceDesignMatrix <- function(seqs,length=25){
+sequenceDesignMatrix <- function(seqs){
   if(length(unique(sapply(seqs,nchar)))!=1) stop("Sequences must be of same length.")
   oligolength <- nchar(seqs[1])
-  mat <- .Call("gcrma_getSeq2",paste(seqs,collapse=""),length(seqs),length,PACKAGE="oligo")
+  mat <- .Call("gcrma_getSeq2",paste(seqs,collapse=""),length(seqs),oligolength,PACKAGE="oligo")
   colnames(mat) <- paste(rep(c("A","C","G"),rep(oligolength,3)),position=rep(1:oligolength,3),sep="_")
   return(mat)
 }
