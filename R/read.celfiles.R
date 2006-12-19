9ae9375c0214faea6cd670a0c6a6a1e6425d54fe
Seth Falcon <sfalcon@fhcrc.org>
2006-12-19 02:09:47 +0000

    Refactor read.celfiles
    Start to make this function aware of the PDInfo classes and use
    methods instead of '@' where possible.
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligo@21571 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/read.celfiles.R
+++ R/read.celfiles.R
@@ -28,8 +28,6 @@
 ##                added feature to return number of pixels (BC)
 #############################################################
 read.celfiles <- function(filenames,
-                          pdenv=TRUE,
-                          arrayType=NULL,
                           pkgname=NULL,
                           sd=FALSE,
                           npixels=FALSE,
@@ -41,108 +39,107 @@ read.celfiles <- function(filenames,
                           compress= FALSE,
                           rm.mask = FALSE,
                           rm.outliers=FALSE,
-                          rm.extra=FALSE){
+                          rm.extra=FALSE) {
 
-  if (!pdenv & is.null(arrayType))
-    stop("You chose not to load the pdenv, so you are required to define arrayType (SNP/expression)")
+    ## FIXME: this is not an acceptable function name
+    tmp <- stuffForXYSandCELreaders(filenames, phenoData, description, notes,
+                                    verbose)
+    filenames <- tmp$filenames
+    n <- length(filenames)
+    if (n == 0)
+      stop("no file name given")
 
-  if (!is.null(pkgname))
-    pdenv <- FALSE
-  
-  tmp <- stuffForXYSandCELreaders(filenames, phenoData, description, notes, verbose)
-  filenames <- tmp$filenames
-  n <- length(filenames)
-  if (n == 0)
-    stop("No file name given !")
+    ## read the first file to see what we have
+    headdetails <- .Call("ReadHeader", filenames[1], compress, PACKAGE="affyio")
 
-  ## read the first file to see what we have
-  headdetails <- .Call("ReadHeader", filenames[1], compress, PACKAGE="affyio")
+    ##now we use the length
+    dim.intensity <- headdetails[[2]]
 
-  ##now we use the length
-  dim.intensity <- headdetails[[2]]
+    ##and the cdfname as ref
+    ref.cdfName <- headdetails[[1]]
 
-  ##and the cdfname as ref
-  ref.cdfName <- headdetails[[1]]
+    ## RI: We should check if the pd package is available here. If not try
+    ## to install it
 
-  ## RI: WE SHOULD CHECK IF THE PD PACKAGE IS AVAILABLE HERE. IF not TRY TO
-  ## INSTALL IT
-
-  ## if pdenv is to be loaded or user specifies alt.pdenv
-  if (pdenv || !is.null(pkgname)){
+    ## load pdInfo
     if (is.null(pkgname))
       pkgname <- cleanPlatformName(ref.cdfName)
     library(pkgname, character.only=TRUE)
-    cat("PDEnv loaded.\n")
-    if (is.null(arrayType))
-      arrayType <- get(pkgname)@type
-  }
-  
-  tmpExprs <- .Call("read_abatch", as.list(filenames), 
-                    rm.mask, rm.outliers, rm.extra, ref.cdfName,
-                    dim.intensity, verbose, PACKAGE="affyio")
+    message("Platform design info loaded.")
+    arrayType <- kind(get(pkgname))
 
-  tmpNP <- tmpSD <- NULL
-  
-  if (sd){
-    tmpSD <- .Call("read_abatch_stddev", as.list(filenames),
-                   rm.mask, rm.outliers, rm.extra, ref.cdfName,
-                   dim.intensity, verbose, PACKAGE="affyio")
-  }
+    tmpExprs <- .Call("read_abatch", as.list(filenames),
+                      rm.mask, rm.outliers, rm.extra, ref.cdfName,
+                      dim.intensity, verbose, PACKAGE="affyio")
 
-  if (npixels){
-    tmpNP <- .Call("read_abatch_npixels", as.list(filenames),
-                   rm.mask, rm.outliers, rm.extra, ref.cdfName,
-                   dim.intensity, verbose, PACKAGE="affyio")
-  }
-  
-  if (pdenv || !is.null(pkgname)){
+    tmpNP <- tmpSD <- NULL
+
+    if (sd){
+        tmpSD <- .Call("read_abatch_stddev", as.list(filenames),
+                       rm.mask, rm.outliers, rm.extra, ref.cdfName,
+                       dim.intensity, verbose, PACKAGE="affyio")
+    }
+
+    if (npixels){
+        tmpNP <- .Call("read_abatch_npixels", as.list(filenames),
+                       rm.mask, rm.outliers, rm.extra, ref.cdfName,
+                       dim.intensity, verbose, PACKAGE="affyio")
+    }
+
     ## BC: Nov 15-16 2005, the PDenv is ordered already in the way
     ##     we want PM/MMs to be. So, we need to reorder the cel
     ##     input, so the pm/mm methods are faster.
-    order_index <- get(pkgname, pos=paste("package:", pkgname, sep=""))$order_index
+    pdInfo <- get(pkgname)
+    if (is(pdInfo, "platformDesign")) {
+        order_index <- pdInfo$order_index
+    } else if (is(pdInfo, "DBPDInfo")) {
+        ## FIXME, need to figure out how to reorder in a useful
+        ## way for the SQLite-based PDInfo stuff
+        order_index <- 1:nrow(tmpExprs)
+    } else {
+        stop("unknown platform info type: ", class(pdInfo))
+    }
     tmpExprs <- tmpExprs[order_index,, drop=FALSE]
     rownames(tmpExprs) <- 1:nrow(tmpExprs)
 
     if (sd){
-      tmpSD <- tmpSD[order_index,, drop=FALSE]
-      rownames(tmpSD) <- as.character(get(pkgname, pos=paste("package:", pkgname, sep=""))$feature_set_name)
+        tmpSD <- tmpSD[order_index,, drop=FALSE]
+        rownames(tmpSD) <- featureSetNames(pdInfo)
     }
 
     if (npixels){
-      tmpNP <- tmpNP[order_index,, drop=FALSE]
-      rownames(tmpNP) <- as.character(get(pkgname, pos=paste("package:", pkgname, sep=""))$feature_set_name)
+        tmpNP <- tmpNP[order_index,, drop=FALSE]
+        rownames(tmpNP) <- featureSetNames(pdInfo)
     }
-  }
 
-  colnames(tmpExprs) <- sapply(strsplit(colnames(tmpExprs), "/"), function(x) x[length(x)])
-  ArrayType <- get(pkgname, pos=paste("package:", pkgname, sep=""))@type
-  if(ArrayType == "tiling"){
-    TheClass <- "TilingFeatureSet"
-  }else if(ArrayType == "expression"){
-    TheClass <- "ExpressionFeatureSet"
-  }else if(ArrayType == "SNP"){
-    TheClass <- "SnpFeatureSet"
-  }else if(ArrayType == "exon"){
-    TheClass <- "ExonFeatureSet"
-  }else{
-    stop(paste("oligo does not know how to handle", ArrayType, "arrays."))
-  }
 
-  if (is.null(featureData))
-    featureData <- new("AnnotatedDataFrame",
-                       data=data.frame(idx=1:nrow(tmpExprs), row.names=1:nrow(tmpExprs)),
-                       varMetadata=data.frame(varLabels="idx", row.names="idx"))
-  out <- new(TheClass,
-             exprs=tmpExprs,
- ##            sd=tmpSD,
- ##            npixels=tmpNP,
-             platform=pkgname,
-             manufacturer="Affymetrix",
-             phenoData=tmp$phenoData,
-             featureData=featureData,
-             experimentData=tmp$description,
-             annotation=substr(pkgname, 3, nchar(pkgname)))
-  return(out)
+    colnames(tmpExprs) <- sapply(strsplit(colnames(tmpExprs), "/"),
+                                 function(x) x[length(x)])
+
+    theClass <- switch(arrayType,
+                       tiling="TilingFeatureSet",
+                       expression="ExpressionFeatureSet",
+                       SNP="SnpFeatureSet",
+                       exon="ExonFeatureSet",
+                       stop("unknown array type: ", arrayType))
+
+    if (is.null(featureData))
+      featureData <- new("AnnotatedDataFrame",
+                         data=data.frame(idx=1:nrow(tmpExprs),
+                           row.names=1:nrow(tmpExprs)),
+                         varMetadata=data.frame(varLabels="idx",
+                           row.names="idx"))
+    out <- new(theClass,
+               exprs=tmpExprs,
+               ##            sd=tmpSD,
+               ##            npixels=tmpNP,
+               platform=pkgname,
+               manufacturer="Affymetrix",
+               phenoData=tmp$phenoData,
+               featureData=featureData,
+               experimentData=tmp$description,
+               annotation=pkgname)
+    out
 }
 
 read.affybatch <- function(..., filenames=character(0),
@@ -152,30 +149,29 @@ read.affybatch <- function(..., filenames=character(0),
                            compress = getOption("BioC")$affy$compress.cel,
                            rm.mask = FALSE, rm.outliers=FALSE, rm.extra=FALSE,
                            verbose = FALSE) {
+    .Deprecated("read.celfiles")
 
-  warning("read.affybatch is depricated. Please use read.celfiles.")
+    auxnames <- as.list(substitute(list(...)))[-1]
+    filenames <- .Primitive("c")(filenames, auxnames)
 
-  auxnames <- as.list(substitute(list(...)))[-1]
-  filenames <- .Primitive("c")(filenames, auxnames)
+    n <- length(filenames)
 
-  n <- length(filenames)
+    ## error if no file name !
+    if (n == 0)
+      stop("No file name given !")
 
-  ## error if no file name !
-  if (n == 0)
-    stop("No file name given !")
-
-  read.celfiles(filenames,
-                phenoData=phenoData,
-                description=description,
-                notes=notes,
-                verbose = verbose,
-                rm.mask = rm.mask,
-                rm.outliers= rm.outliers,
-                rm.extra=rm.extra)
+    read.celfiles(filenames,
+                  phenoData=phenoData,
+                  description=description,
+                  notes=notes,
+                  verbose = verbose,
+                  rm.mask = rm.mask,
+                  rm.outliers= rm.outliers,
+                  rm.extra=rm.extra)
 }
 
 list.celfiles <-   function(...){
-  files <- list.files(...)
-  return(files[grep("\\.[cC][eE][lL]\\.gz$|\\.[cC][eE][lL]$", files)])
+    files <- list.files(...)
+    return(files[grep("\\.[cC][eE][lL]\\.gz$|\\.[cC][eE][lL]$", files)])
 }
 
