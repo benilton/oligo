4bf6766ccd15367d803d0f3857c606cbf369140b
Benilton Carvalho <bcarvalh@jhsph.edu>
2007-08-21 03:55:00 +0000

    Adding a missing file
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligo@26544 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/methods-SnpCnvFeatureSet.R
@@ -0,0 +1,26 @@
+setMethod("pmindex", "AffySNPCNVPDInfo",
+          function(object, probes.type="snp") {
+            ## might improve by telling RSQLite how
+            ## many rows we will fetch?  same for mmindex.
+            if (probes.type == "snp"){
+              dbGetQuery(db(object),
+                         "select fid from pmfeature")[[1]]
+            }else if (probes.type == "cnv"){
+              dbGetQuery(db(object),
+                         "SELECT fid FROM pmfeatureCNV ORDER BY fid")[[1]]
+            }
+          })
+
+setMethod("pmindex", "SnpCnvFeatureSet",
+          function(object, probes.type="snp"){
+            pmindex(getPlatformDesign(object), probes.type=probes.type)
+          })
+
+setMethod("pm", "SnpCnvFeatureSet",
+          function(object, genenames=NULL, probes.type="snp"){
+            if (!is.null(genenames)) message("genenames ignored (not implemented yet)")
+            tmp <- subBufferedMatrix(exprs(object), pmindex(object, probes.type=probes.type))
+            RowMode(tmp)
+            set.buffer.dim(tmp, 50000, 1)
+            return(tmp)
+          })
