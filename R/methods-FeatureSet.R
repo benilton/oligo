b5eb69299edc74cc6503e7f4a90c67ed0a84b3b5
Benilton Carvalho <bcarvalh@jhsph.edu>
2009-02-02 22:04:50 +0000

    Fixed getPlatformDesign method
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligo@37082 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-FeatureSet.R
+++ R/methods-FeatureSet.R
@@ -1,87 +1,11 @@
-## setMethod("initialize", signature(.Object="FeatureSet"), 
-##           function(.Object,
-##                    assayData = assayDataNew(exprs=exprs, ...),
-##                    manufacturer=as.character(NA),
-##                    platform=as.character(NA),
-##                    exprs=matrix(numeric(0), nrow=nrow, ncol=ncol),
-##                    phenoData = annotatedDataFrameFrom(assayData, byrow=FALSE),
-##                    featureData = annotatedDataFrameFrom(assayData, byrow=TRUE),
-##                    experimentData = new("MIAME"),
-##                    annotation = new("character"), ...){
-##             .Object <- callNextMethod(.Object,
-##                                       assayData = assayData,
-##                                       phenoData = phenoData,
-##                                       experimentData = experimentData,
-##                                       annotation = annotation,
-##                                       featureData = featureData)
-##             .Object@manufacturer <- manufacturer
-##             .Object@platform <- platform
-##             .Object
-##           })
-## 
-## 
-## setMethod("platform", signature(object="FeatureSet"),function(object) object@platform)
-## setReplaceMethod("platform",signature(object="FeatureSet"),
-## 		  function(object,value) {
-## 			  object@platform <- value
-## 			  object
-## 		  })
-##   
-## setMethod("manufacturer", signature(object="FeatureSet"), function(object) object@manufacturer)
-## setReplaceMethod("manufacturer", signature(object="FeatureSet"), 
-## 		function(object, value){
-##   			object@manufacturer <- value
-##   			object
-## })
-## 
-## setMethod("length", signature(x="FeatureSet"), function(x) nrow(pData(x)))
-
-## Currently dim, ncol and nrow all pull from eSet Class definition, and thus is the size of
-##  exprs matrix and not array, should change to array dims -- MS
-# setMethod("dim",
-#          signature=signature(object="FeatureSet"),
-#          function(object) c(object@nrow, object@ncol))
-#
-
-
-## changed annotation to platform - MS
-## thinking is that platform is akin to cdf and annotation akin to db
-## currently they are both the same package, but may in the future want to have annotationDBI like packages
-## for these arrays as well and use annotation slot for this
-
-##loading the library for now... this must change, why?
 setMethod("getPlatformDesign", 
-        signature(object= "FeatureSet"),
-        function(object){
-            pdn <- platform(object)
+          signature(object= "FeatureSet"),
+          function(object){
+            pdn <- annotation(object)
             library(pdn,character.only=TRUE)
             return(get(pdn,pos=paste("package:",pdn,sep="")))
-})
+          })
 getPD <- getPlatformDesign
-
-
-## setMethod("exprs",
-## 		signature(object="FeatureSet"),
-## 		function(object) assayDataElement(object,"exprs"))
-## setReplaceMethod("exprs",
-## 		signature(object="FeatureSet"),
-## 		function(object, value) assayDataElementReplace(object, "exprs", value))
-
-### future 
-setMethod("se.exprs",
-		signature(object="FeatureSet"),
-		function(object) {
-			obj <- assayDataElement(object,"se.exprs")
-			if (is.null(obj))
-				new("matrix")
-			else
-				obj
-		})
-### future
-setReplaceMethod("se.exprs",
-		signature(object="FeatureSet"),
-		function(object, value) assayDataElementReplace(object, "se.exprs", value))
-
 
 #####################
 ############# STRANGE, what should they be?
