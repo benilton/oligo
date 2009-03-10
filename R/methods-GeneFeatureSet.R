34cd2c83db80f0a8437457f2744f265f8ef45e34
Benilton Carvalho <bcarvalh@jhsph.edu>
2009-03-10 17:44:36 +0000

    Much better support to Exon/Gene Arrays
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligo@37873 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/methods-GeneFeatureSet.R
@@ -0,0 +1,59 @@
+## RMA here is simply done to the probeset-level
+## b/c the db structure is different (1:N mapping from probe to probeset)
+## I'm reimplementing the method for RMA/Selecting the probes
+
+setMethod("rma", "GeneFeatureSet",
+          function(object, background=TRUE, normalize=TRUE, subset=NULL){
+            conn <- db(object)
+
+            ## This is the only difference from RMA for Exon
+            sql <- paste("SELECT fid, fsetid FROM pmfeature INNER JOIN f2fset USING(fid)")
+            
+            featureInfo <- dbGetQuery(conn, sql)
+            featureInfo <- featureInfo[order(featureInfo[["fsetid"]]),]
+            rownames(featureInfo) <- NULL
+            pms <- exprs(object)[featureInfo[["fid"]],, drop=FALSE]
+            dimnames(pms) <- NULL
+            theExprs <- basicRMA(pms,
+                                 as.character(featureInfo[["fsetid"]]),
+                                 length(unique(featureInfo[["fsetid"]])),
+                                 normalize, background)
+            rm(pms)
+
+            ## Getting exon and gene info to add to featureData
+            sql <- paste("SELECT fsetid, exon_id, transcript_cluster_id,",
+                         "level, crosshyb_type, chrom",
+                         "FROM featureSet")
+            fromFSetTable <- dbGetQuery(conn, sql)
+            sql <- paste("SELECT fsetid, group_concat(accession) as accessions",
+                         "FROM fset2gene",
+                         "INNER JOIN gene USING(gid)",
+                         "GROUP BY fsetid")
+            fromFSet2Gene <- dbGetQuery(conn, sql)
+            fromFSet2Gene[["accessions"]] <- sapply(strsplit(fromFSet2Gene[["accessions"]], ","),
+                                                    function(x) paste(sort(x), collapse=","))
+            probesets <- merge(fromFSetTable, fromFSet2Gene, by="fsetid", all.x=TRUE)
+            rm(sql, fromFSetTable, fromFSet2Gene)
+            idx <- probesets[["fsetid"]] %in% unique(featureInfo[["fsetid"]])
+            probesets <- probesets[idx,]
+            rm(idx)
+            
+            descs <- c("probeset id", "exon id", "transcript cluster id",
+                       "level", "cross hybridization type", "chromosome",
+                       "accessions")
+            vmd <- data.frame(labelDescription=descs)
+            rm(descs)
+            rownames(probesets) <- as.character(probesets[["fsetid"]])
+            tmp <- new("AnnotatedDataFrame", data=probesets,
+                       varMetadata=vmd)
+            rm(vmd, probesets)
+
+            out <- new("ExpressionSet",
+                       featureData=tmp,
+                       phenoData=phenoData(object),
+                       annotation=annotation(object),
+                       experimentData=experimentData(object),
+                       exprs=theExprs)
+            rm(tmp)
+            return(out)
+          })
