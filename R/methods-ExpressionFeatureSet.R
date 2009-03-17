7e12a08e4b8304e23bc8abe7d122f88f1bec367b
Benilton Carvalho <bcarvalh@jhsph.edu>
2009-03-17 22:58:44 +0000

    Added support to HT_HGU_133+ Array
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligo@38009 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-ExpressionFeatureSet.R
+++ R/methods-ExpressionFeatureSet.R
@@ -1,18 +1,17 @@
 setMethod("rma", "ExpressionFeatureSet",
           function(object, background=TRUE, normalize=TRUE, subset=NULL){
             pms <- pm(object, subset)
-            if (manufacturer(object) == "Affymetrix"){
-              tmpQcPm <- dbGetQuery(db(object),
-                                    paste("SELECT man_fsetid, fid",
-                                          "FROM featureSet, qcpmfeature",
-                                          "WHERE qcpmfeature.fsetid=featureSet.fsetid"))
-              qcpms <- exprs(object)[tmpQcPm[["fid"]],]
-
-              pms <- rbind(pms, qcpms)
-            }
             pnVec <- probeNames(object, subset)
-            if (manufacturer(object) == "Affymetrix")
+            if (manufacturer(object) == "Affymetrix"){
+              sql <- paste("SELECT man_fsetid, fid",
+                           "FROM bgfeature",
+                           "INNER JOIN featureSet",
+                           "USING(fsetid)")
+              tmpQcPm <- dbGetQuery(db(object), sql)
+              qcpms <- exprs(object)[tmpQcPm[["fid"]],]
+              pms <- rbind(pms, qcpms)
               pnVec <- c(pnVec, tmpQcPm[["man_fsetid"]])
+            }
             ngenes <- length(unique(pnVec))
             idx <- order(pnVec)
             pms <- pms[idx,, drop=FALSE]
