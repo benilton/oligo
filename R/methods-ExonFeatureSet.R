880d20a20504e11b3da6de07783232b4da0dd2e2
Benilton Carvalho <bcarvalh@jhsph.edu>
2009-08-06 22:23:36 +0000

    Improving support to Gene/Exon Arrays - gene level summaries
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligo@41065 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-ExonFeatureSet.R
+++ R/methods-ExonFeatureSet.R
@@ -1,51 +1,26 @@
 setMethod("rma", "ExonFeatureSet",
-          function(object, background=TRUE, normalize=TRUE, subset=NULL){
-            conn <- db(object)
-            sql <- paste("SELECT fid, fsetid FROM pmfeature")
-            featureInfo <- dbGetQuery(conn, sql)
-            featureInfo <- featureInfo[order(featureInfo[["fsetid"]]),]
-            rownames(featureInfo) <- NULL
+          function(object, background=TRUE, normalize=TRUE, subset=NULL, target="core"){
+            if (target == "core"){
+              featureInfo <- getFidMetaProbesetCore(object)
+            }else if (target == "full"){
+              featureInfo <- getFidMetaProbesetFull(object)
+            }else if (target == "extended"){
+              featureInfo <- getFidMetaProbesetExtended(object)
+            }else if (target == "probeset"){
+              featureInfo <- getFidProbeset(object)
+            }else{
+              stop("Target '", target, "' is invalid. It must be one of: 'core', 'full', 'extended' or 'probeset'.")
+            }
             pms <- exprs(object)[featureInfo[["fid"]],, drop=FALSE]
             dimnames(pms) <- NULL
             theExprs <- basicRMA(pms,
                                  as.character(featureInfo[["fsetid"]]),
                                  normalize, background)
             rm(pms)
-
-            ## Getting exon and gene info to add to featureData
-            sql <- paste("SELECT fsetid, exon_id, transcript_cluster_id,",
-                         "level, crosshyb_type, chrom",
-                         "FROM featureSet")
-            fromFSetTable <- dbGetQuery(conn, sql)
-            sql <- paste("SELECT fsetid, group_concat(accession) as accessions",
-                         "FROM fset2gene",
-                         "INNER JOIN gene USING(gid)",
-                         "GROUP BY fsetid")
-            fromFSet2Gene <- dbGetQuery(conn, sql)
-            fromFSet2Gene[["accessions"]] <- sapply(strsplit(fromFSet2Gene[["accessions"]], ","),
-                                                    function(x) paste(sort(x), collapse=","))
-            probesets <- merge(fromFSetTable, fromFSet2Gene, by="fsetid", all.x=TRUE)
-            rm(sql, fromFSetTable, fromFSet2Gene)
-            idx <- probesets[["fsetid"]] %in% unique(featureInfo[["fsetid"]])
-            probesets <- probesets[idx,]
-            rm(idx)
-            
-            descs <- c("probeset id", "exon id", "transcript cluster id",
-                       "level", "cross hybridization type", "chromosome",
-                       "accessions")
-            vmd <- data.frame(labelDescription=descs)
-            rm(descs)
-            rownames(probesets) <- as.character(probesets[["fsetid"]])
-            tmp <- new("AnnotatedDataFrame", data=probesets,
-                       varMetadata=vmd)
-            rm(vmd, probesets)
-
             out <- new("ExpressionSet",
-                       featureData=tmp,
                        phenoData=phenoData(object),
                        annotation=annotation(object),
                        experimentData=experimentData(object),
                        exprs=theExprs)
-            rm(tmp)
             return(out)
           })
--- R/utils.R
+++ R/utils.R
@@ -305,6 +305,53 @@ getNgsColorsInfo <- function(path=".", pattern1="_532", pattern2="_635", ...){
   return(out)
 }
 
+## Selectors for probes - useful for exon/gene arrays
+
+getFidProbeset <- function(object){
+  conn <- db(object)
+  sql <- "SELECT fid, fsetid FROM pmfeature"
+  featureInfo <- dbGetQuery(conn, sql)
+  bgInfo <- dbGetQuery(conn, "SELECT fid, fsetid FROM bgfeature")
+  featureInfo <- rbind(featureInfo, bgInfo)
+  featureInfo <- featureInfo[order(featureInfo[["fsetid"]]),]
+  rownames(featureInfo) <- NULL
+  return(featureInfo)
+}
+
+getFidMetaProbesetCore <- function(object){
+  conn <- db(object)
+  sql <- "SELECT fid, meta_fsetid as fsetid FROM pmfeature INNER JOIN core_mps USING(fsetid)"
+  featureInfo <- dbGetQuery(conn, sql)
+  bgInfo <- dbGetQuery(conn, "SELECT fid, fsetid FROM bgfeature")
+  featureInfo <- rbind(featureInfo, bgInfo)
+  featureInfo <- featureInfo[order(featureInfo[["fsetid"]]),]
+  rownames(featureInfo) <- NULL
+  return(featureInfo)
+}
+
+getFidMetaProbesetFull <- function(object){
+  conn <- db(object)
+  sql <- "SELECT fid, meta_fsetid as fsetid FROM pmfeature INNER JOIN full_mps USING(fsetid)"
+  featureInfo <- dbGetQuery(conn, sql)
+  bgInfo <- dbGetQuery(conn, "SELECT fid, fsetid FROM bgfeature")
+  featureInfo <- rbind(featureInfo, bgInfo)
+  featureInfo <- featureInfo[order(featureInfo[["fsetid"]]),]
+  rownames(featureInfo) <- NULL
+  return(featureInfo)
+}
+
+getFidMetaProbesetExtended <- function(object){
+  conn <- db(object)
+  sql <- "SELECT fid, meta_fsetid as fsetid FROM pmfeature INNER JOIN extended_mps USING(fsetid)"
+  featureInfo <- dbGetQuery(conn, sql)
+  bgInfo <- dbGetQuery(conn, "SELECT fid, fsetid FROM bgfeature")
+  featureInfo <- rbind(featureInfo, bgInfo)
+  featureInfo <- featureInfo[order(featureInfo[["fsetid"]]),]
+  rownames(featureInfo) <- NULL
+  return(featureInfo)
+}
+
+
 ## Date/time extractors
 GetAffyTimeDateAsString <- function(filenames, useAffyio=TRUE){
   if (useAffyio){
