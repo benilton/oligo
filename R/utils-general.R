976226ebdc9588f67c6db548a62d441103eeebe4
Benilton Carvalho <bcarvalh@jhsph.edu>
2010-02-01 15:55:11 +0000

    Fixed selection of bg probes on Exon/Gene chips so it obeys the MPS files
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligo@44435 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/utils-general.R
+++ R/utils-general.R
@@ -320,33 +320,70 @@ getFidProbeset <- function(object){
   return(featureInfo)
 }
 
+## getFidMetaProbesetCore <- function(object){
+##   conn <- db(object)
+##   sql <- "SELECT fid, meta_fsetid as fsetid FROM pmfeature INNER JOIN core_mps USING(fsetid)"
+##   featureInfo <- dbGetQuery(conn, sql)
+##   bgInfo <- dbGetQuery(conn, "SELECT fid, fsetid FROM bgfeature")
+##   featureInfo <- rbind(featureInfo, bgInfo)
+##   featureInfo <- featureInfo[order(featureInfo[["fsetid"]]),]
+##   rownames(featureInfo) <- NULL
+##   return(featureInfo)
+## }
+
 getFidMetaProbesetCore <- function(object){
   conn <- db(object)
   sql <- "SELECT fid, meta_fsetid as fsetid FROM pmfeature INNER JOIN core_mps USING(fsetid)"
   featureInfo <- dbGetQuery(conn, sql)
-  bgInfo <- dbGetQuery(conn, "SELECT fid, fsetid FROM bgfeature")
+  sql <- "SELECT fid, meta_fsetid as fsetid FROM bgfeature INNER JOIN core_mps USING(fsetid)"
+  bgInfo <- dbGetQuery(conn, sql)
   featureInfo <- rbind(featureInfo, bgInfo)
   featureInfo <- featureInfo[order(featureInfo[["fsetid"]]),]
   rownames(featureInfo) <- NULL
   return(featureInfo)
 }
+
+
+## getFidMetaProbesetFull <- function(object){
+##   conn <- db(object)
+##   sql <- "SELECT fid, meta_fsetid as fsetid FROM pmfeature INNER JOIN full_mps USING(fsetid)"
+##   featureInfo <- dbGetQuery(conn, sql)
+##   bgInfo <- dbGetQuery(conn, "SELECT fid, fsetid FROM bgfeature")
+##   featureInfo <- rbind(featureInfo, bgInfo)
+##   featureInfo <- featureInfo[order(featureInfo[["fsetid"]]),]
+##   rownames(featureInfo) <- NULL
+##   return(featureInfo)
+## }
 
 getFidMetaProbesetFull <- function(object){
   conn <- db(object)
   sql <- "SELECT fid, meta_fsetid as fsetid FROM pmfeature INNER JOIN full_mps USING(fsetid)"
   featureInfo <- dbGetQuery(conn, sql)
-  bgInfo <- dbGetQuery(conn, "SELECT fid, fsetid FROM bgfeature")
+  sql <- "SELECT fid, meta_fsetid as fsetid FROM bgfeature INNER JOIN full_mps USING(fsetid)"
+  bgInfo <- dbGetQuery(conn, sql)
   featureInfo <- rbind(featureInfo, bgInfo)
   featureInfo <- featureInfo[order(featureInfo[["fsetid"]]),]
   rownames(featureInfo) <- NULL
   return(featureInfo)
 }
 
+## getFidMetaProbesetExtended <- function(object){
+##   conn <- db(object)
+##   sql <- "SELECT fid, meta_fsetid as fsetid FROM pmfeature INNER JOIN extended_mps USING(fsetid)"
+##   featureInfo <- dbGetQuery(conn, sql)
+##   bgInfo <- dbGetQuery(conn, "SELECT fid, fsetid FROM bgfeature")
+##   featureInfo <- rbind(featureInfo, bgInfo)
+##   featureInfo <- featureInfo[order(featureInfo[["fsetid"]]),]
+##   rownames(featureInfo) <- NULL
+##   return(featureInfo)
+## }
+
 getFidMetaProbesetExtended <- function(object){
   conn <- db(object)
   sql <- "SELECT fid, meta_fsetid as fsetid FROM pmfeature INNER JOIN extended_mps USING(fsetid)"
   featureInfo <- dbGetQuery(conn, sql)
-  bgInfo <- dbGetQuery(conn, "SELECT fid, fsetid FROM bgfeature")
+  sql <- "SELECT fid, meta_fsetid as fsetid FROM bgfeature INNER JOIN extended_mps USING(fsetid)"
+  bgInfo <- dbGetQuery(conn, sql)
   featureInfo <- rbind(featureInfo, bgInfo)
   featureInfo <- featureInfo[order(featureInfo[["fsetid"]]),]
   rownames(featureInfo) <- NULL
