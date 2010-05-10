15ea89367548b20223c9a8b669c8f234d1b2b42c
Benilton Carvalho <bcarvalh@jhsph.edu>
2010-05-10 17:03:42 +0000

    Adding gzipped XYS support
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligo@46772 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/ParserXYS.c
+++ src/ParserXYS.c
@@ -30,6 +30,7 @@
 #include <R.h>
 #include <Rdefines.h>
 #include <Rinternals.h>
+#include <zlib.h>
 
 #define LINEMAX 20001
 #define WORD 32
@@ -337,3 +338,267 @@ SEXP R_read_xys_header(SEXP filename){
   untokenizer(header);
   return(output);
 }
+
+/*
+Trying to read gzipped XYS files
+*/
+
+static int isgzXysFile(const char *filename){
+ gzFile currentFile = NULL; 
+ char buffer[LINEMAX];
+ char *token;
+ currentFile = gzopen(filename, "r");
+ if (currentFile == NULL){
+   error("Could not open file %s", filename);
+ } else {
+   /** check if file starts with "# software=NimbleScan" **/
+   gzgets(currentFile, buffer, LINEMAX);
+   gzgets(currentFile, buffer, LINEMAX);
+
+   // X
+   token = strtok(buffer, " \t");
+   if (token[0] != 'X'){
+     gzclose(currentFile);
+     return 0;
+   }
+
+   // Y
+   token = strtok(NULL, " \t");
+   if (token[0] != 'Y'){
+     gzclose(currentFile);
+     return 0;
+   }
+
+   // SIGNAL
+   token = strtok(NULL, " \t");
+   if (strncmp("SIGNAL", token, 5) != 0){
+     gzclose(currentFile);
+     return 0;
+   }
+
+   // COUNT
+   token = strtok(NULL, " \t");
+   if (strncmp("COUNT", token, 4) != 0){
+     gzclose(currentFile);
+     return 0;
+   }
+
+   // 3 fields
+   gzgets(currentFile, buffer, LINEMAX);
+   token = strtok(buffer, " \t");
+   if (token == NULL){
+     gzclose(currentFile);
+     return 0;
+   }
+
+   token = strtok(NULL, " \t");
+   if (token == NULL){
+     gzclose(currentFile);
+     return 0;
+   }
+
+   token = strtok(NULL, " \t");
+   if (token == NULL){
+     gzclose(currentFile);
+     return 0;
+   }
+
+
+ }
+ return 1;
+}
+
+
+/***************************************************************
+ ** countLines: counts lines, returns integer
+***************************************************************/
+static int countLinesGz(gzFile *file){
+  int lines = 0;
+  char buffer[LINEMAX];
+  while (!gzeof(file)){
+    gzgets(file, buffer, LINEMAX);
+    lines++;
+  }
+  return lines;
+}
+
+
+/***************************************************************
+ ** xys_header_field: gets a field in the header of an XYS file.
+ **
+ ** NOTE: user must deallocate memory
+ **       after using this function!!!!
+***************************************************************/
+
+static char *gzxys_header_field(const char *currentFile, const char *field){
+  gzFile *fp;
+  int j;
+  char *result, *final;
+  char buffer[LINEMAX];
+
+  fp = gzopen(currentFile, "r");
+  if (fp == NULL)
+    error("Can't open %s.\n", currentFile);
+
+  gzgets(fp, buffer, LINEMAX);
+  gzclose(fp);
+
+  j = strlen(buffer)-1;
+  if (buffer[j] == '\n')
+    buffer[j] = '\0';
+  
+  result = strstr(buffer, field);
+  if (result == NULL)
+    error("Can't find \'%s\' field. %s corrupted?", field, currentFile);
+  result = strtok(result, "=");
+  result = strtok(NULL, "\t");
+  final = Calloc(strlen(result)+1, char);
+  strcpy(final, result);
+  return final;
+}
+
+/***************************************************************
+ ** read_one_xys: reads XYS file. Stores XY coordinates on 'xy'
+ **               and intensities on 'signal'
+***************************************************************/
+
+
+static void read_one_gzxys(const char *filename, double *signal,
+			   int *xy, int i, int nrow, int verbose){
+  int count;
+  //  char sx[WORD], sy[WORD], ss[WORD], sc[WORD], *endpx, *endpy, *endps;
+  char buffer[LINEMAX];
+  char *token;
+  gzFile fp=NULL;
+
+  if (verbose) Rprintf("Reading %s.\n", filename);
+  fp = gzopen(filename, "r");
+  if (fp == NULL)
+    error("Can't open %s.\n", filename);
+
+  // Header - 2 lines - skip
+  while (gzgetc(fp) != '\n');
+  while (gzgetc(fp) != '\n');
+  count = 0;
+  while (!gzeof(fp)){
+    Rprintf("Line %d\n", count);
+    gzgets(fp, buffer, LINEMAX);
+
+    // Get X only for the 1st file
+    token = strtok(buffer, " \t");
+    Rprintf("Trying to read X\n");
+    if (token == NULL)
+      error("Incomplete line found.");
+    if (i == 0)
+      xy[count] = atol(token);
+    
+    // Get Y only for the 1st file
+    token = strtok(NULL, " \t");
+    Rprintf("Trying to read Y\n");
+    if (token == NULL)
+      error("Incomplete line found.");
+    if (i == 0)
+      xy[count + nrow] = atol(token);
+
+    // Get Signal for every file
+    token = strtok(NULL, " \t");
+    Rprintf("Trying to read SIGNAL\n");
+    if (token == NULL)
+      error("Incomplete line found.");
+    if (token[0] != 'N'){
+      signal[count + i*nrow] = atof(token);
+    }else{ // NA_REAL is R-specific
+      signal[count + i*nrow] = NA_REAL;
+    }
+    
+    count++;
+  }
+  gzclose(fp);
+}
+
+SEXP R_read_gzxys_files(SEXP filenames, SEXP verbosity){
+  int nfiles, nrows, i, verbose, *ptr2xy;
+  double *ptr2signal;
+  gzFile *fp;
+  SEXP signal, xy, output;
+  SEXP dimnames, dimnamesxy, fnames, colnamesxy, namesout, dates;
+  char *d0, *d1;
+
+  verbose = asLogical(verbosity);
+  nfiles = length(filenames);
+  fp = gzopen(CHAR(STRING_ELT(filenames, 0)), "r");
+  if (fp == NULL)
+    error("Can't open %s.\n", CHAR(STRING_ELT(filenames, 0)));
+  nrows = countLinesGz(fp)-2;
+  gzclose(fp);
+
+  // Test files are of the same type here
+  if (verbose) Rprintf("Checking designs for each XYS file... ");
+  d0 = gzxys_header_field(CHAR(STRING_ELT(filenames, 0)), "designname=");
+  if (nfiles > 1)
+    for (i = 1; i < nfiles; i++){
+      d1 = gzxys_header_field(CHAR(STRING_ELT(filenames, i)), "designname=");
+      if(strcasecmp(d1, d0) != 0){
+	Free(d0);
+	Free(d1);
+	error("\'%s\' and \'%s\' use different designs.\n",
+	      CHAR(STRING_ELT(filenames, 0)),
+	      CHAR(STRING_ELT(filenames, i)));
+      }
+      Free(d1); // Missed: 12/02/09
+    }
+  Free(d0);
+  if (verbose) Rprintf("Done.\n");
+
+  // Allocating memory in R
+  if (verbose) Rprintf("Allocating memory... ");
+  PROTECT(signal = allocMatrix(REALSXP, nrows, nfiles));
+  PROTECT(xy = allocMatrix(INTSXP, nrows, 2));
+  PROTECT(dates = allocVector(STRSXP, nfiles));
+  if (verbose) Rprintf("Done.\n");
+  ptr2xy = INTEGER_POINTER(xy);
+  ptr2signal = NUMERIC_POINTER(signal);
+
+  // Parsing files here
+  for (i=0; i < nfiles; i++){
+    Rprintf("Detected as GZ: %d\n", isgzXysFile(CHAR(STRING_ELT(filenames, i))));
+    read_one_gzxys(CHAR(STRING_ELT(filenames, i)), ptr2signal,
+		 ptr2xy, i, nrows, verbose);
+    d0 = gzxys_header_field(CHAR(STRING_ELT(filenames, i)), "date=");
+    SET_STRING_ELT(dates, i, mkChar(d0));
+    Free(d0);
+  }
+
+  PROTECT(output = allocVector(VECSXP, 3));
+  SET_VECTOR_ELT(output, 0, xy);
+  SET_VECTOR_ELT(output, 1, signal);
+  SET_VECTOR_ELT(output, 2, dates);
+
+  // Dimnames +5 PROTECTs
+  PROTECT(dimnames = allocVector(VECSXP, 2));
+  PROTECT(fnames = allocVector(STRSXP, nfiles));
+  for (i=0; i < nfiles; i++)
+    SET_STRING_ELT(fnames, i, mkChar(CHAR(STRING_ELT(filenames, i))));
+  SET_VECTOR_ELT(dimnames, 1, fnames);
+  SET_VECTOR_ELT(dimnames, 0, R_NilValue);
+  setAttrib(signal, R_DimNamesSymbol, dimnames);
+  setAttrib(dates, R_NamesSymbol, fnames);
+
+  PROTECT(dimnamesxy = allocVector(VECSXP, 2));
+  PROTECT(colnamesxy = allocVector(STRSXP, 2));
+  SET_STRING_ELT(colnamesxy, 0, mkChar("X"));
+  SET_STRING_ELT(colnamesxy, 1, mkChar("Y"));
+  SET_VECTOR_ELT(dimnamesxy, 0, R_NilValue);
+  SET_VECTOR_ELT(dimnamesxy, 1, colnamesxy);
+  setAttrib(xy, R_DimNamesSymbol, dimnamesxy);
+
+  PROTECT(namesout = allocVector(STRSXP, 3));
+  SET_STRING_ELT(namesout, 0, mkChar("coordinates"));
+  SET_STRING_ELT(namesout, 1, mkChar("intensities"));
+  SET_STRING_ELT(namesout, 2, mkChar("date"));
+  setAttrib(output, R_NamesSymbol, namesout);
+
+  UNPROTECT(9);
+  return(output);
+}
+
