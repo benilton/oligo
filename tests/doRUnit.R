c40ec669c4c15aea0ee2471336aba8ab91b86148
Benilton Carvalho <bcarvalh@jhsph.edu>
2013-07-30 18:56:45 +0000

    Adding 1st unit test
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligo@78981 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/doRUnit.R
+++ tests/doRUnit.R
@@ -1,6 +1,7 @@
 ## from xmapcore package
 if( require( "RUnit", quietly=TRUE ) ) {
-	pkg <- "oligo"
+message("There")
+  pkg <- "oligo"
 
         path <- ifelse(Sys.getenv( "RCMDCHECK" ) == "FALSE",
                        file.path( getwd(), "..", "inst", "unitTests" ),
@@ -35,5 +36,6 @@ if( require( "RUnit", quietly=TRUE ) ) {
 		stop( paste( "\n\nunit testing failed (#test failures: ", tmp$nFail, ", #R errors: ",  tmp$nErr, ")\n\n", sep=""))
 	}
 } else {
+  message("Here")
 	warning( "cannot run unit tests -- package RUnit is not available" )
 }
