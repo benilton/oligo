5b86d579e2a762a3fc8220468c7cb4ef39b3cb07
Benilton Carvalho <bcarvalh@jhsph.edu>
2013-07-30 19:20:09 +0000

    Fixed rownames -> NULL; added progress messages
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligo@79068 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_rma.R
+++ inst/unitTests/test_rma.R
@@ -1,14 +1,17 @@
 test_rma <- function(){
   library(oligo)
   library(RUnit)
+  message('Getting sample dataset')
   xysPath <- system.file("extdata", package="maqcExpression4plex")
   xysFiles <- list.xysfiles(xysPath, full.name=TRUE)
   ngsExpressionFeatureSet <- read.xysfiles(xysFiles)
+  message('Running RMA')
   summarized <- rma(ngsExpressionFeatureSet)
   rm(ngsExpressionFeatureSet)
   load(system.file('unitTests', 'i0.rda', package='oligo'))
   t0 <- exprs(summarized)[i0,]
   rm(summarized)
+  message('Getting reference results')
   load(system.file('unitTests', 'rma_ref0.rda', package='oligo'))
   checkEquals(rma_ref0, t0)
 }
