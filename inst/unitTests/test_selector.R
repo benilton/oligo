67db628836474a687d2f5ea5ca04ed1454872a0c
Benilton Carvalho <bcarvalh@jhsph.edu>
2013-07-30 19:20:27 +0000

    Fixing probe subsetting
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligo@79069 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_selector.R
+++ inst/unitTests/test_selector.R
@@ -3,33 +3,34 @@ test_selector <- function(){
     library(oligoData)
     library(RUnit)
     pkgs <- c('pd.huex.1.0.st.v2', 'pd.hugene.1.0.st.v1')
-    message('Loading reference data')
-    load(system.file('unitTests', 'fids_ref0.rda', package='oligo'))
+    getRandom <- function(dt){
+        set.seed(1)
+        i <- sort(sample(nrow(dt), 100))
+        d0 <- dt[i,]
+        rownames(d0) <- NULL
+        d0
+    }
     fld <- c('fid', 'fsetid', 'type')
     out <- c('fid', 'man_fsetid')
-    message('Loading sample dataset: Exon')
     data(affyExonFS)
-    message('Getting probe info: core')
-    coreExon <- getProbeInfo(affyExonFS, field=fld, target='core')[out][icore0[[pkgs[1]]],]
-    message('Getting probe info: probeset')
-    psetExon <- getProbeInfo(affyExonFS, field=fld, target='probeset')[out][ipset0[[pkgs[1]]],]
-    message('Getting probe info: antigenomic backgroung probes')
-    agenExon <- getProbeInfo(affyExonFS, field=fld, target='probeset', subset= type == 'control->bgp->antigenomic')[out][iagen0[[pkgs[1]]],]
-    rownames(coreExon) <- rownames(psetExon) <- rownames(agenExon) <- NULL
+    coreExon <- getProbeInfo(affyExonFS, field=fld, target='core')[out]
+    coreExon <- getRandom(coreExon)
+    psetExon <- getProbeInfo(affyExonFS, field=fld, target='probeset')[out]
+    psetExon <- getRandom(psetExon)
+    agenExon <- getProbeInfo(affyExonFS, field=fld, target='probeset', subset= type == 'control->bgp->antigenomic')[out]
+    agenExon <- getRandom(agenExon)
 
-    message('Loading sample dataset: Gene')
     data(affyGeneFS)
-    message('Getting probe info: core')
-    coreGene <- getProbeInfo(affyGeneFS, field=fld, target='core')[out][icore0[[pkgs[2]]],]
-    message('Getting probe info: probeset')
-    psetGene <- getProbeInfo(affyGeneFS, field=fld, target='probeset')[out][ipset0[[pkgs[2]]],]
-    message('Getting probe info: antigenomic backgroung probes')
-    agenGene <- getProbeInfo(affyGeneFS, field=fld, target='probeset', subset= type == 'control->bgp->antigenomic')[out][iagen0[[pkgs[2]]],]
-    rownames(coreGene) <- rownames(psetGene) <- rownames(agenGene) <- NULL
-
+    coreGene <- getProbeInfo(affyGeneFS, field=fld, target='core')[out]
+    coreGene <- getRandom(coreGene)
+    psetGene <- getProbeInfo(affyGeneFS, field=fld, target='probeset')[out]
+    psetGene <- getRandom(psetGene)
+    agenGene <- getProbeInfo(affyGeneFS, field=fld, target='probeset', subset= type == 'control->bgp->antigenomic')[out]
+    agenGene <- getRandom(agenGene)
     core <- list(coreExon, coreGene)
     pset <- list(psetExon, psetGene)
     agen <- list(agenExon, agenGene)
     names(core) <- names(pset) <- names(agen) <- pkgs
+    load(system.file('unitTests', 'fids_ref0.rda', package='oligo'))
     checkEquals(core, core0) & checkEquals(pset, pset0) & checkEquals(agen, agen0)
 }
